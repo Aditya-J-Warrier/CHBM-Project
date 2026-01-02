afde0af7a34792c870ec36b5b79df01a2f39e3d9
Aditya Jai Warrier <adityajaiwarrier@gmail.com>
2026-01-02 13:39:11 +0100

    Initial CHBM frontal impact setup based on VIVA model

---
+++ CHBM_FrontalImpact_Aditya/runcommand.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\LSTC\LS-DYNA\R09.31\ls-dyna_smp_s_R9_3_1_winx64_ifort160.exe" i=D:\Simulations\ViVA\FrontalImpact_SimplifiedNeck\run_frontal_impact.key ncpu=4 memory=51m > lsrun.out.txt 2>&1
